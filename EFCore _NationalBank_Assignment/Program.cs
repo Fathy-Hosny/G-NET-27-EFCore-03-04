2b325664d29f42062caa7db3f3ce43daa3617b5b
Fathy Hosny <ali01120939266@gmail.com>
2026-05-03 13:21:46 +0300

    Add project files.

---
+++ EFCore _NationalBank_Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore__NationalBank_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
